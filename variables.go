132dcba64b855bfbda7e6a0c0bd761994b67a7b0
Tiago Costa <tiagodpv@gmail.com>
2024-02-07 18:33:39 -0500

    thursay

--- variables.go
+++ variables.go
@@ -4,36 +4,47 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramentos = 2
+const delay = 5
 
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando() 
+	
 
-	/*if comando == 1 {                 // if no golang sempre retorna booleano (true or false)
-		fmt.Println("Monitorando..")
-	} else if comando == 2 {
-		fmt.Println("Exibindo logs..")
-	} else if comando == 0 {
-		fmt.Println("Saindo do Programa")
-	} else {
-		fmt.Println("Não conheço este comando")
-	}*/
+	for {
+		exibeMenu()
+		comando := leComando() 
 
-	switch comando {
-		case 1:			
-			iniciarMonitoramento()
-		case 2:
+		/*if comando == 1 {                 // if no golang sempre retorna booleano (true or false)
+			fmt.Println("Monitorando..")
+		} else if comando == 2 {
 			fmt.Println("Exibindo logs..")
-		case 0:
+		} else if comando == 0 {
 			fmt.Println("Saindo do Programa")
-			os.Exit(0)
-		default:
+		} else {
 			fmt.Println("Não conheço este comando")
-			os.Exit(-1) // indica que ocorreu algum erro no programa
+		}*/
+	
+		switch comando {
+			case 1:			
+				iniciarMonitoramento()
+			case 2:
+				fmt.Println("Exibindo logs..")
+			case 0:
+				fmt.Println("Saindo do Programa")
+				os.Exit(0)
+			default:
+				fmt.Println("Não conheço este comando")
+				os.Exit(-1) // indica que ocorreu algum erro no programa
+		}
+
 	}
+
+	
 }
 
 
@@ -55,20 +66,42 @@ func leComando() int {
 	fmt.Scan(&comandoLido)
 	fmt.Println("O endereço da minha variavel comando é", &comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
 	return comandoLido
 }
 
 
 func iniciarMonitoramento() {
-	fmt.Println("1- Iniciar Monitoramento")
-	site := "https://www.alura.com.br"
+	fmt.Println("Monitorando...")	
+	sites:= []string{"https://httpbin.org/status/40", "https://www.alura.com.br", "https://www.caelum.com.br"}
+	
+	//example using for loop
+	/*for i:=0; i< len(sites); i++ {
+		fmt.Println(sites[i])		
+	}*/
+
+	for i:=0; i< monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+	
+	fmt.Println("")
+	
+	
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
-	fmt.Println(resp)	 
 
 	if resp.StatusCode == 200 {
-		fmt.Println("Site:", site, "foi carregado com sucesso. Status Code:", resp.StatusCode)
-	}else{
-		fmt.Println("Site:", site, "Está com problemas. Status Code:", resp.StatusCode)
+			fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+			fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
 	}
+
 }
